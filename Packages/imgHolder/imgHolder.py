af0dc1b66d945f8fedddfbace04b21168fd3cd43
fisker <lionkay@gmail.com>
2016-11-21 18:41:12 +0800

    add imgholder

---
+++ Packages/imgHolder/imgHolder.py
@@ -0,0 +1,24 @@
+import sublime, sublime_plugin, re
+
+
+class imgHolder(sublime_plugin.EventListener):
+    def on_query_completions(self, view, prefix, locations):
+        syntax = view.settings().get('syntax')
+        settings = sublime.load_settings("imgHolder.sublime-settings")
+        imgholders = settings.get("imgholders")
+        url_prefix = settings.get("urlprefix")
+
+        def pos(pos):
+            point = view.sel()[0].begin()
+            return view.substr(sublime.Region(point - pos, point))
+
+        showCompletions = []
+
+        attr = pos(5)
+
+        for s in (u'html', u'css', u'less', u'sass', u'scss'):
+            if syntax.lower().find(s):
+                if attr.find('src=') != -1 or attr.find('url(') != -1:
+                    showCompletions = [(url_prefix + ih, url_prefix + '${1:' + ih + '}') for ih in imgholders]
+            return showCompletions
+        return
